3f1b26ef81551c5cca67cb6877a12e9c0425bf40
Rico-blib <git@github.com>
2022-06-08 07:52:10 +0300

    project changes

--- README.md
+++ README.md
@@ -1,23 +1,13 @@
-# Rust Smart Contract Template
+# Workouts Project
+## .The project generates random work out plans.
+## .The user should enter the intensity value and the program generates a work out plan.
+### .Some test are also included in the program
 
-## Getting started
 
-To get started with this template:
-
-1. Click the "Use this template" button to create a new repo based on this template
-2. Update line 2 of `Cargo.toml` with your project name
-3. Update line 4 of `Cargo.toml` with your project author names
-4. Set up the [prerequisites](https://github.com/near/near-sdk-rs#pre-requisites)
-5. Begin writing your smart contract in `src/lib.rs`
-6. Test the contract 
-
-    `cargo test -- --nocapture`
-
-8. Build the contract
-
-    `RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release`
-
-**Get more info at:**
-
-* [Rust Smart Contract Quick Start](https://docs.near.org/docs/develop/contracts/rust/intro)
-* [Rust SDK Book](https://www.near-sdk.io/)
+# Software used
+## 1.rust 
+## 2.cargo 
+## 3.Near Cli
+ 
+# Authors
+## .Erick Mburu
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,145 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::collections::Vector;
+use near_sdk::{env, near_bindgen};
+near_sdk::setup_alloc!();
+use std::collections::HashMap;
+
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    // SETUP CONTRACT STATE
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Workout {
+    users_work_out: HashMap<String, i8>,
+}
+
+impl Default for Workout {
+    fn default() -> Self {
+        Self {
+            users_work_out: HashMap::new(),
+        }
+    }
 }
 
 #[near_bindgen]
-impl Contract {
-    // ADD CONTRACT METHODS HERE
+impl Workout {
+    pub fn get_rate(&self) -> Option<&i8> {
+        self.users_work_out.get(&env::signer_account_id())
+    }
+
+    // user work out intensity
+    // set by the user
+    pub fn create_workout(&mut self, rate: i8) {
+        self.users_work_out.insert(env::signer_account_id(), rate);
+    }
+
+    // user utility function to update user workout
+    // in hashmap doc if you insert a value with the same key the value is updated
+    pub fn update_intensity(&mut self, rate: i8) {
+        self.users_work_out.insert(env::signer_account_id(), rate);
+    }
+
+    // get a list of user work out
+    #[result_serializer(borsh)]
+    pub fn get_workout(&self) -> Vector<String> {
+        let account = env::signer_account_id();
+        let intensity = self.users_work_out.get(&account);
+        let mut wrk: Vector<String> = Vector::new(b"m");
+        match intensity {
+            Some(value) => match value {
+                1..=10 => {
+                    wrk.push(&String::from("15 push up"));
+                    wrk.push(&String::from("next,50 jumps"));
+                    wrk.push(&String::from("break for 1 minute then do 10 situps"));
+                }
+                11..=20 => {
+                    wrk.push(&String::from("20 situps"));
+                    wrk.push(&String::from("next,run for 30 minutes"));
+                    wrk.push(&String::from("next frog jump for 5 minutes"));
+                }
+                21..=30 => {
+                    wrk.push(&String::from("30 press ups"));
+                    wrk.push(&String::from("next,duckwalk for 10 minutes"));
+                    wrk.push(&String::from("next,jog for 5 minutes"));
+                }
+                31..=40 => {
+                    wrk.push(&String::from("frog jump for 5 minutes"));
+                    wrk.push(&String::from("next,jog for 10 minutes"));
+                    wrk.push(&String::from("take a 1 minute break,do 20 pullups"));
+                }
+
+                _ => {
+                    wrk.push(&String::from("run for 1 hour"));
+                    wrk.push(&String::from("next,60 jumps"));
+                    wrk.push(&String::from("frog jump for 20 minutes"));
+                }
+            },
+
+            None => {
+                env::log(b"You have not registered intensity, you get the default workouts");
+
+                wrk.push(&String::from("20 push up"));
+            }
+        }
+
+        return wrk;
+    }
 }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+    //set up a mock context
+    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+        VMContext {
+            current_account_id: "alice.testnet".to_string(),
+            signer_account_id: "robert.testnet".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: "jane.testnet".to_string(),
+            input,
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 0,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view,
+            output_data_receivers: vec![],
+            epoch_height: 19,
+        }
+    }
+    #[test]
+    fn create_workout() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        //initiate  a contract variable
+        let mut contract = Workout::default();
+        contract.create_workout(12);
 
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+        let intensity = match contract.get_rate() {
+            Some(k) => k,
+            None => &0,
+        };
+        assert_eq!(&12, intensity);
     }
 
-    // TESTS HERE
+    #[test]
+    fn update_workout() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        //initiate  a contract variable
+        let mut contract = Workout::default();
+        contract.create_workout(12);
+
+        contract.update_intensity(15);
+
+        let intensity = match contract.get_rate() {
+            Some(k) => k,
+            None => &0,
+        };
+        assert_eq!(&15, intensity);
+    }
+
 }
